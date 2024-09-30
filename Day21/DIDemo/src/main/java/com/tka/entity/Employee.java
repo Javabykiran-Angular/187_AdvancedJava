00c5c2f946141c1017284c57492f39bcadc74f77
The Kiran Academy <71135924+Javabykiran-Angular@users.noreply.github.com>
2024-09-30 11:55:18 +0530

    Day21

---
+++ Day21/DIDemo/src/main/java/com/tka/entity/Employee.java
@@ -0,0 +1,59 @@
+package com.tka.entity;
+
+public class Employee {
+	
+	int id;
+	String name;
+	
+	Country country;
+	
+	public Employee() {
+		
+	}
+	
+	public Employee(String name) {
+		super();
+		this.name = name;
+	}
+	public Employee(String name,int id) {
+		super();
+		this.name = name;
+		this.id=id;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId1(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName1(String name) {
+		this.name = name;
+	}
+	
+	
+	
+	public Country getCountry() {
+		return country;
+	}
+
+	public void setCountry(Country country) {
+		this.country = country;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", country=" + country + "]";
+	}
+
+//	@Override
+//	public String toString() {
+//		return "Employee [id=" + id + ", name=" + name + "]";
+//	}
+//	
+	
+	
+
+}
